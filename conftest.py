0e803afcc6e85c394d3979e3ed46f954217aa8d4
Keryn Knight <keryn@kerynknight.com>
2015-03-21 17:06:02 +0000

    prerequisites

---
+++ conftest.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+import django
+from django.conf import settings
+
+def pytest_configure():
+    if not settings.configured:
+        settings.configure(
+            DATABASES={
+                "default": {
+                    "ENGINE": "django.db.backends.sqlite3",
+                    "NAME": ":memory:"
+                    }
+                },
+            INSTALLED_APPS=(
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+            ),
+            MIDDLEWARE_CLASSES=(),
+        )
+    if hasattr(django, 'setup'):
+        django.setup()
